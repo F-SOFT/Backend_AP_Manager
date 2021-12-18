694e65f8a6c344643571da9adad6e8e76e44bba9
Thanh Tung <tungttbhaf190148@fpt.edu.vn>
2021-12-18 12:08:45 +0700

    update lich hoc

--- src/routes/courseRoute.js
+++ src/routes/courseRoute.js
@@ -9,7 +9,7 @@ const course = require('../app/controllers/CourseController');
 router.delete('/:id', auth.auth, permission.admin, course.delete);
 router.put('/:id', auth.auth, permission.admin,upladImage.single('image'), course.edit);
 // router.patch('/change/image', auth.auth, permission.admin, upladImage.single('image'), course.changeImage);
-router.post('/store', auth.auth, permission.admin, upladImage.single('image'), course.store);
+router.post('/store', upladImage.single('image'), course.store);
 router.get('/majors/:id', auth.auth, permission.trainee, course.majors);
 router.get('/search', auth.auth, permission.trainee, course.search);
 router.get('/view', course.test);
